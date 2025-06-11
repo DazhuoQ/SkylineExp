e79d36b8a072713e96c5e5a1dc66ad481c12957b
anthonyche <hxc859@case.edu>
2025-06-10 20:55:49 -0400

    arxiv

--- src/utils.py
+++ src/utils.py
@@ -5,10 +5,11 @@ import yaml
 import torch
 import torch_geometric.transforms as T
 from torch_geometric.datasets import Planetoid, FacebookPagePage, AmazonProducts, Amazon
-# from ogb.nodeproppred import PygNodePropPredDataset
+from ogb.nodeproppred import PygNodePropPredDataset
 from torch_geometric.datasets import ExplainerDataset
 from torch_geometric.datasets.graph_generator import BAGraph
 from torch_geometric.utils import remove_self_loops
+
 
 
 def set_seed(seed):
